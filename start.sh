f150b8d86e6ce56fb804d82cb833a744b011dfeb
Jσєℓ Bσвαη [Oғғlιɴe] <95572329+JoelBobanOffline@users.noreply.github.com>
2022-05-01 08:36:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
-echo "Cloning Repository"
-git clone https://<pat>@github.com/BXBotz2021/Spider-Man.git
-cd Spider-Man
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/BXBotz2021/Spider-Man.git /Spidey
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Spidey
+fi
+cd /Spidey
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+python3 main.py
